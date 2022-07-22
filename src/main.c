59437a4a6c96dcdf20eb69c152227773a9b26fcb
scuderia666 <satouuxd@gmail.com>
2022-07-22 19:06:40 +0300

    up

--- src/main.c
+++ src/main.c
@@ -6,7 +6,9 @@ int screenHeight = 450;
 
 void UpdateDrawFrame(void);
 
-typedef enum GameScreen { LOGO = 0, TITLE, GAMEPLAY, ENDING } GameScreen;
+typedef enum GameScreen {
+	LOGO = 0, TITLE, GAMEPLAY, ENDING
+} GameScreen;
 
 GameScreen currentScreen = LOGO;
 
@@ -14,21 +16,18 @@ bool hasPopup = false;
 
 Vector2 mousePoint = { 0.0f, 0.0f };
 
-int main()
-{
+int main() {
     InitWindow(screenWidth, screenHeight, "game");
 
     SetTargetFPS(60);
 
-    while (!WindowShouldClose())
-    {
+    while (!WindowShouldClose()) {
 	BeginDrawing();
 	mousePoint = GetMousePosition();
-	if(hasPopup){
+	if (hasPopup) {
 		DrawPopup();
 	}
-	switch(currentScreen)
-        {
+	switch (currentScreen) {
             case LOGO:
 		Menu();
 		break;
@@ -50,7 +49,7 @@ Vector2 vel = {0, 0};
 
 int speed = 1000;
 
-#define MAX_INPUT_CHARS     9
+#define MAX_INPUT_CHARS 9
 
 char name[MAX_INPUT_CHARS + 1] = "\0";
 int letterCount = 0;
@@ -65,11 +64,13 @@ Rectangle no = { 300, 200, 200, 100 };
 Rectangle ok = { 360, 250, 75, 45 };
 char popupText[64] = "\0";
 
-typedef enum PopupType { WARNING = 0, CHOOSE } PopupType;
+typedef enum PopupType {
+	WARNING = 0, CHOOSE
+} PopupType;
 
 PopupType popupType = LOGO;
 
-void SendPopup(char* text){
+void SendPopup(char* text) {
 	popupType = WARNING;
 	char *ptr = text;
 	int i = 0;
@@ -82,17 +83,17 @@ void SendPopup(char* text){
 	hasPopup = true;
 }
 
-void DrawPopup(void){
+void DrawPopup(void) {
 	DrawRectangleRec(popupBox, BLUE);
 	DrawText(popupText, popupBox.x, popupBox.y+25, 20, DARKBLUE);
 	if (popupType == CHOOSE){
 		DrawRectangleRec(yes, GREEN);
 		DrawRectangleRec(no, RED);
-	}else{
+	} else {
 		DrawRectangleRec(ok, GREEN);
 		DrawText("ok", ok.x, ok.y+15, 14, DARKBLUE);
 		bool act = false;
-		if (CheckCollisionPointRec(mousePoint, ok)){
+		if (CheckCollisionPointRec(mousePoint, ok)) {
 			if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) act = true;
 		}
 		if (act) {
@@ -103,20 +104,19 @@ void DrawPopup(void){
 	}
 }
 
-void Menu(void)
-{
+void Menu(void) {
 	ClearBackground(RAYWHITE);
         bool btnAction = false;
 	Rectangle rect = { 50, 50, 50, 50 };
 	DrawRectangleRec(rect, RED);
-	if (CheckCollisionPointRec(mousePoint, rect) && !hasPopup){
+	if (CheckCollisionPointRec(mousePoint, rect) && !hasPopup) {
 		if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) btnAction = true;
 	}
-	if(btnAction){
+	if (btnAction) {
 		btnAction = false;
-		if(letterCount == 0){
-			SendPopup("bir isim yaz aptal.");
-		}else{
+		if (letterCount == 0) {
+			SendPopup("bir isim yaz");
+		} else {
 			currentScreen = GAMEPLAY;
 		}
 		return;
@@ -125,16 +125,14 @@ void Menu(void)
 	bool mouseOnText = false;
 	if (CheckCollisionPointRec(GetMousePosition(), textBox) && !hasPopup) mouseOnText = true;
         else mouseOnText = false;
-	if(mouseOnText){
+	if (mouseOnText) {
 		SetMouseCursor(MOUSE_CURSOR_IBEAM);
 		int key = GetCharPressed();
 
             // Check if more characters have been pressed on the same frame
-            while (key > 0)
-            {
+            while (key > 0) {
                 // NOTE: Only allow keys in range [32..125]
-                if ((key >= 32) && (key <= 125) && (letterCount < MAX_INPUT_CHARS))
-                {
+                if ((key >= 32) && (key <= 125) && (letterCount < MAX_INPUT_CHARS)) {
                     name[letterCount] = (char)key;
                     name[letterCount+1] = '\0'; // Add null terminator at the end of the string.
                     letterCount++;
@@ -143,7 +141,7 @@ void Menu(void)
                 key = GetCharPressed();  // Check next character in the queue
             }
 
-	    if (deleteLetter){
+	    if (deleteLetter) {
                 letterCount--;
                 if (letterCount < 0) letterCount = 0;
                 name[letterCount] = '\0';
@@ -152,27 +150,26 @@ void Menu(void)
 
             if (IsKeyDown(KEY_BACKSPACE) && delay == 0)
 		deleteLetter = true;
-	}else{
+	} else {
 		SetMouseCursor(MOUSE_CURSOR_DEFAULT);
 	}
-	DrawText("neym", textBox.x+50, textBox.y-30, 20, DARKBLUE);
+	DrawText("nick", textBox.x+50, textBox.y-30, 20, DARKBLUE);
 	DrawRectangleRec(textBox, LIGHTGRAY);
             if (mouseOnText && !hasPopup) DrawRectangleLines((int)textBox.x, (int)textBox.y, (int)textBox.width, (int)textBox.height, RED);
             else DrawRectangleLines((int)textBox.x, (int)textBox.y, (int)textBox.width, (int)textBox.height, DARKGRAY);
 	DrawText(name, (int)textBox.x + 5, (int)textBox.y + 8, 20, MAROON);
 }
 
-void Game(void)
-{
+void Game(void) {
         ClearBackground(RED);
 
 	float delta = GetFrameTime();
 
-	if (!hasPopup){
-	if (IsKeyDown(KEY_D)) vel.x += speed*delta;
-        else if (IsKeyDown(KEY_A)) vel.x -= speed*delta;
-	if (IsKeyDown(KEY_S)) vel.y += speed*delta;
-        else if (IsKeyDown(KEY_W)) vel.y -= speed*delta;
+	if (!hasPopup) {
+		if (IsKeyDown(KEY_D)) vel.x += speed*delta;
+        	else if (IsKeyDown(KEY_A)) vel.x -= speed*delta;
+		if (IsKeyDown(KEY_S)) vel.y += speed*delta;
+        	else if (IsKeyDown(KEY_W)) vel.y -= speed*delta;
 	}
 
 	pos.x = Lerp(pos.x, vel.x, 0.1);
